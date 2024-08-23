05cd70f59ad8e7667e8f1fea523537bd83071a2d
Jay McPartland <jay@bigbite.net>
2024-08-23 17:18:48 +0100

    refactor: pull blockProps up and use more granular attribute data

--- src/blocks/image-comparison-item/components/Image.js
+++ src/blocks/image-comparison-item/components/Image.js
@@ -39,15 +39,15 @@ function imageUrl(image, size) {
  * Renders image in various states
  *
  * @param {object}   props                 Component props
- * @param {object}   props.blockProps      Block generated props
- * @param {object}   props.attributes      Block attributes
+ * @param {number}   props.id              The image ID
+ * @param {string}   props.sizeSlug        The requested image size
  * @param {string}   props.alternativeText Alternative image text
  * @param {function} props.setAttributes   Update block attributes
  */
-const Image = ({ blockProps, attributes, alternativeText, setAttributes }) => {
+const Image = ({ alternativeText, id, sizeSlug, setAttributes }) => {
   const media = useSelect(
-    (select) => attributes?.id && select(coreStore).getMedia(attributes.id, { context: 'view' }),
-    [attributes],
+    (select) => id && select(coreStore).getMedia(id, { context: 'view' }),
+    [id],
   );
 
   const [src, setSrc] = useState();
@@ -56,42 +56,38 @@ const Image = ({ blockProps, attributes, alternativeText, setAttributes }) => {
     // Try to use the previous selected image size if its available
     // otherwise fallback to "full"
     let size = 'full';
-    if (attributes?.sizeSlug && hasSize(media, attributes?.sizeSlug)) {
-      size = attributes?.sizeSlug;
+    if (sizeSlug && hasSize(media, sizeSlug)) {
+      size = sizeSlug;
     }
 
     setSrc(imageUrl(media, size));
-  }, [media, attributes?.sizeSlug]);
+  }, [media, sizeSlug]);
 
   if (media?.id && src) {
-    // eslint-disable-next-line react/jsx-props-no-spreading -- recommended usage of blockProps
-    return <img {...blockProps} src={src} alt={alternativeText} />;
+    return <img src={src} alt={alternativeText} />;
   }
 
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading -- recommended usage of blockProps
-    <div {...blockProps}>
-      <MediaUploadCheck>
-        <MediaUpload
-          value={{ id: media?.id, src }}
-          allowedTypes={['image']}
-          onSelect={({ id }) => setAttributes({ id })}
-          render={({ open }) => (
-            <Flex
-              className="wp-block-bigbite-image-comparison-item__uploader"
-              direction="column"
-              justify="center"
-            >
-              <FlexItem>
-                <Button variant="primary" onClick={open}>
-                  {__('Select Image', 'bigbite-image-comparison')}
-                </Button>
-              </FlexItem>
-            </Flex>
-          )}
-        />
-      </MediaUploadCheck>
-    </div>
+    <MediaUploadCheck>
+      <MediaUpload
+        value={{ id: media?.id, src }}
+        allowedTypes={['image']}
+        onSelect={(item) => setAttributes({ id: item.id })}
+        render={({ open }) => (
+          <Flex
+            className="wp-block-bigbite-image-comparison-item__uploader"
+            direction="column"
+            justify="center"
+          >
+            <FlexItem>
+              <Button variant="primary" onClick={open}>
+                {__('Select Image', 'bigbite-image-comparison')}
+              </Button>
+            </FlexItem>
+          </Flex>
+        )}
+      />
+    </MediaUploadCheck>
   );
 };
 
