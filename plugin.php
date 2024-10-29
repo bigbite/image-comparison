c25aaa38b1d2eef83700c2365619e41abfe4a6ab
Chris <66952080+chrishbite@users.noreply.github.com>
2024-10-29 15:25:58 +0000

    refactor: updates codebase for new build-tools version

--- plugin.php
+++ plugin.php
@@ -22,27 +22,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'BIGBITE_IMAGE_COMPARISON_PLUGIN_DIR', __DIR__ );
 
-if ( file_exists( BIGBITE_IMAGE_COMPARISON_PLUGIN_DIR . '/inc/asset-settings.php' ) ) {
-	include_once BIGBITE_IMAGE_COMPARISON_PLUGIN_DIR . '/inc/asset-settings.php';
-}
-
-if (
-	! defined( 'BIGBITE_IMAGE_COMPARISON_EDITOR_CSS' ) ||
-	! defined( 'BIGBITE_IMAGE_COMPARISON_EDITOR_JS' ) ||
-	! defined( 'BIGBITE_IMAGE_COMPARISON_SHARED_CSS' ) ||
-	! defined( 'BIGBITE_IMAGE_COMPARISON_FRONTEND_CSS' ) ||
-	! defined( 'BIGBITE_IMAGE_COMPARISON_EDITOR_DEPENDENCIES' ) ||
-	! defined( 'BIGBITE_IMAGE_COMPARISON_VERSION' )
-) {
-	add_action(
-		'all_admin_notices',
-		function () {
-			printf(
-				'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
-				esc_html__( 'Image Comparison asset constants are not defined. You may need to run an asset build.', 'bigbite-image-comparison' )
-			);
-		}
-	);
-
-	return;
-}
+require_once BIGBITE_IMAGE_COMPARISON_PLUGIN_DIR . '/inc/class-loader.php';
+new Loader( BIGBITE_IMAGE_COMPARISON_PLUGIN_DIR );
