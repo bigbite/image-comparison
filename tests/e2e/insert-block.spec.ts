d312cf2d46581239562d7e4d862c0e14a66603dd
Richard Walker <well@thatisrich.co.uk>
2024-11-27 11:17:32 +0000

    feat: expands initial block test

--- tests/e2e/insert-block.spec.ts
+++ tests/e2e/insert-block.spec.ts
@@ -13,6 +13,9 @@ test.describe('Image Comparison block', () => {
     await page.keyboard.type('Test Image Comparison Block');
   });
 
+  /**
+   * Test to ensure the block can be fully populated and rendered
+   */
   test('should be added to a page, populated, and rendered', async ({ editor, page }) => {
     await test.step('add the block to the page', async () => {
       await editor.insertBlock({ name: 'bigbite/image-comparison' });
@@ -115,6 +118,182 @@ test.describe('Image Comparison block', () => {
 
       // Expect the scan to have no issues
       expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  /**
+   * Test to ensure if the block is not populated, it is not rendered
+   */
+
+  test('should be added to a page with no images', async ({ editor, page }) => {
+    await test.step('add the block to the page', async () => {
+      await editor.insertBlock({ name: 'bigbite/image-comparison' });
+
+      // expect block to be visible on the page
+      const imageComparisonBlock = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .locator('.wp-block-bigbite-image-comparison');
+
+      // Assert the block is visible
+      await expect(imageComparisonBlock).toBeVisible();
+    });
+
+    await test.step('add an image into slot 1', async () => {
+      await page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByRole('button', { name: 'Select Image' })
+        .first()
+        .click();
+
+      await page.getByLabel('before').click();
+      await page.getByRole('button', { name: 'Select', exact: true }).click();
+
+      // Ensure the first image is added to the block
+      const firstComparisonImage = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByLabel('Block: Image Comparison Item')
+        .first();
+      await expect(firstComparisonImage).toBeVisible();
+    });
+
+    await test.step('Save and visit the page', async () => {
+      // Save the page
+      await page.getByRole('button', { name: 'Publish', exact: true }).click();
+      await page
+        .getByLabel('Editor publish')
+        .getByRole('button', { name: 'Publish', exact: true })
+        .click();
+      // Visit the page
+      await page.getByTestId('snackbar').getByRole('link', { name: 'View Post' }).click();
+      // Make sure the heading is correct
+      await expect(page.title()).resolves.toMatch('Test Image Comparison Block');
+    });
+
+    // Define the image comparison container
+    const imageComparisonContainer = page.locator('.wp-block-bigbite-image-comparison__container');
+
+    await test.step('Ensure block is not present', async () => {
+      await expect(imageComparisonContainer).not.toBeVisible();
+    });
+  });
+
+  /**
+   * Test to ensure if the block is partially populated (first image only), it is not rendered
+   */
+  test('should be added to a page with the first image only', async ({ editor, page }) => {
+    await test.step('add the block to the page', async () => {
+      await editor.insertBlock({ name: 'bigbite/image-comparison' });
+
+      // expect block to be visible on the page
+      const imageComparisonBlock = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .locator('.wp-block-bigbite-image-comparison');
+
+      // Assert the block is visible
+      await expect(imageComparisonBlock).toBeVisible();
+    });
+
+    await test.step('add an image into slot 1', async () => {
+      await page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByRole('button', { name: 'Select Image' })
+        .first()
+        .click();
+
+      await page.getByLabel('before').click();
+      await page.getByRole('button', { name: 'Select', exact: true }).click();
+
+      // Ensure the first image is added to the block
+      const firstComparisonImage = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByLabel('Block: Image Comparison Item')
+        .first();
+      await expect(firstComparisonImage).toBeVisible();
+    });
+
+    await test.step('Save and visit the page', async () => {
+      // Save the page
+      await page.getByRole('button', { name: 'Publish', exact: true }).click();
+      await page
+        .getByLabel('Editor publish')
+        .getByRole('button', { name: 'Publish', exact: true })
+        .click();
+      // Visit the page
+      await page.getByTestId('snackbar').getByRole('link', { name: 'View Post' }).click();
+      // Make sure the heading is correct
+      await expect(page.title()).resolves.toMatch('Test Image Comparison Block');
+    });
+
+    // Define the image comparison container
+    const imageComparisonContainer = page.locator('.wp-block-bigbite-image-comparison__container');
+
+    await test.step('Ensure block is not present', async () => {
+      await expect(imageComparisonContainer).not.toBeVisible();
+    });
+  });
+
+  /**
+   * Test to ensure if the block is partially populated (second image only), it is not rendered
+   */
+
+  test('should be added to a page with the first second only', async ({ editor, page }) => {
+    await test.step('add the block to the page', async () => {
+      await editor.insertBlock({ name: 'bigbite/image-comparison' });
+
+      // expect block to be visible on the page
+      const imageComparisonBlock = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .locator('.wp-block-bigbite-image-comparison');
+
+      // Assert the block is visible
+      await expect(imageComparisonBlock).toBeVisible();
+    });
+
+    await test.step('add an image into slot 2', async () => {
+      await page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByRole('button', { name: 'Select Image' })
+        .nth(1)
+        .click();
+
+      await page.getByLabel('after').click();
+      await page.getByRole('button', { name: 'Select', exact: true }).click();
+
+      // Ensure the first image is added to the block
+      const secondComparisonImage = page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByLabel('Block: Image Comparison Item')
+        .nth(1);
+      await expect(secondComparisonImage).toBeVisible();
+    });
+
+    await test.step('Save and visit the page', async () => {
+      // Save the page
+      await page.getByRole('button', { name: 'Publish', exact: true }).click();
+      await page
+        .getByLabel('Editor publish')
+        .getByRole('button', { name: 'Publish', exact: true })
+        .click();
+      // Visit the page
+      await page.getByTestId('snackbar').getByRole('link', { name: 'View Post' }).click();
+      // Make sure the heading is correct
+      await expect(page.title()).resolves.toMatch('Test Image Comparison Block');
+    });
+
+    // Define the image comparison container
+    const imageComparisonContainer = page.locator('.wp-block-bigbite-image-comparison__container');
+
+    await test.step('Ensure block is not present', async () => {
+      await expect(imageComparisonContainer).not.toBeVisible();
     });
   });
 });
