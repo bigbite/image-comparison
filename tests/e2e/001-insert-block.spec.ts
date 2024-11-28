5ddfb4b8fb55f450486c886aebd30e438123106e
Richard Walker <well@thatisrich.co.uk>
2024-11-28 11:24:28 +0000

    feat: adds block resize test

--- tests/e2e/001-insert-block.spec.ts
+++ tests/e2e/001-insert-block.spec.ts
@@ -42,7 +42,7 @@ test.describe('Image Comparison block', () => {
   test('should be added to a page, populated, and rendered', async ({ blockFixtures, page }) => {
     await test.step('add an image into slot 1', async () => {
       // Select the before image for use in the first slot
-      await blockFixtures.addFirstImageToBlock('before');
+      await blockFixtures.addImageToAvailableSlot('before');
 
       // Ensure the first image is added to the block
       const firstComparisonImage = page
@@ -58,7 +58,7 @@ test.describe('Image Comparison block', () => {
 
     await test.step('add an image into slot 2', async () => {
       // Select the after image for use in the second slot
-      await blockFixtures.addFirstImageToBlock('after');
+      await blockFixtures.addImageToAvailableSlot('after');
 
       // Ensure the second image is added to the block
       const secondComparisonImage = page
@@ -145,7 +145,7 @@ test.describe('Image Comparison block', () => {
   }) => {
     await test.step('add an image into slot 1', async () => {
       // Select the before image for use in the first slot
-      await blockFixtures.addFirstImageToBlock('before');
+      await blockFixtures.addImageToAvailableSlot('before');
 
       // Ensure the first image is added to the block
       const firstComparisonImage = page
@@ -186,7 +186,7 @@ test.describe('Image Comparison block', () => {
 
     await test.step('add an image into slot 2', async () => {
       // Select the after image for use in the second slot
-      await blockFixtures.addSecondImageToBlock('after');
+      await blockFixtures.addImageToSecondSlot('after');
 
       // Ensure the second image is added to the block
       const secondComparisonImage = page
@@ -220,6 +220,72 @@ test.describe('Image Comparison block', () => {
 
     await test.step('Ensure block is not present', async () => {
       await expect(imageComparisonContainer).not.toBeVisible();
+    });
+  });
+
+  /**
+   * Test to ensure custom block sizes are rendered
+   */
+  test('should be altered and rendered at 250px x 250px', async ({
+    blockFixtures,
+    editor,
+    page,
+  }) => {
+    await test.step('Add the images', async () => {
+      await blockFixtures.addImageToAvailableSlot('before');
+      await blockFixtures.addImageToAvailableSlot('after');
+    });
+
+    await test.step('Adjust the width and height of the block', async () => {
+      // Select the outer block
+
+      await page
+        .locator('iframe[name="editor-canvas"]')
+        .contentFrame()
+        .getByLabel('Block: Image Comparison', { exact: true })
+        .click();
+
+      // Check if the Settings panel is open
+      const isSettingsPanelOpen = await page.getByLabel('Editor settings').isVisible();
+
+      // If not, click the Settings panel button
+      if (!isSettingsPanelOpen) {
+        await page.getByLabel('Settings', { exact: true }).click();
+      }
+
+      // Check if the Block tab is selected
+      const isBlockTabSelected =
+        (await page.getByRole('tab', { name: 'Block' }).getAttribute('aria-selected')) === 'true';
+
+      // If not, select the Block tab
+      if (!isBlockTabSelected) {
+        await page.getByRole('tab', { name: 'Block' }).click();
+      }
+
+      // Set the width and height to 250px
+      await page.getByLabel('Width').fill('250');
+      await page.getByLabel('Height').fill('250');
+    });
+
+    await test.step('Save and visit the page', async () => {
+      await blockFixtures.saveVisitPage();
+
+      // Make sure the heading is correct
+      await expect(page.title()).resolves.toMatch('Test Image Comparison Block');
+    });
+
+    await test.step('Confirm the size of the container is 250px x 250px', async () => {
+      // Define the container
+      const imageComparisonContainer = page.locator(
+        '.wp-block-bigbite-image-comparison__container',
+      );
+
+      // Get the container bounding box
+      const containerBoundingBox = await imageComparisonContainer.boundingBox();
+
+      // Assert the container width and height match the expected values
+      expect(containerBoundingBox?.width).toEqual(250);
+      expect(containerBoundingBox?.height).toEqual(250);
     });
   });
 });
